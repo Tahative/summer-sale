6cedd353dd35db41062db28ba376610647870bd3
Tahative <Tahamindabd1339722@gmail.com>
2023-08-24 22:54:21 +0600

    name added in the list

--- js/coupon.js
+++ js/coupon.js
@@ -17,111 +17,36 @@ document.getElementById('coupon-id').addEventListener(
         }
     }
 );
-// Get all the elements with the class name "btn-card"
-const btnCards = document.querySelectorAll('.btn-card');
-
-// Get the <ol> element where you want to add the list items
-const productContainer = document.getElementById('add-item-name');
-
-// Counter to keep track of the list item number
-let listItemNumber = 1;
-
-// Attach click event listeners to each "btn-card" element
-btnCards.forEach(function(btnCard) {
-  btnCard.addEventListener('click', function() {
-    // Get the product name from the card
-    const productName = btnCard.querySelector('.product-name').textContent;
-
-    // Create a new <li> element with the product name and number
-    const li = document.createElement('li');
-    li.textContent = `${listItemNumber}. ${productName}`;
-
-    // Append the new <li> element to the <ol> container
-    productContainer.appendChild(li);
-
-    // Increment the list item number for the next click
-    listItemNumber+;
-  });
-});
-
-// //   // Add an event listener to the "Make Purchase" button
-// //   const makePurchaseButton = document.querySelector('#btn-make-purchase');
-// //   makePurchaseButton.addEventListener('click', updateTotalPrice);
-
-//   // Function to calculate and update the total price
-//   function updateTotalPrice() {
-//     // Get all the added item names and their prices
-//     const addedItems = document.querySelectorAll('.added-item');
-//     let totalAmount = 0;
-
-//     // Calculate the total price
-//     addedItems.forEach((item) => {
-//       const price = parseFloat(item.getAttribute('data-price'));
-//       totalAmount += price;
-//     });
-
-//     // Update the total price span
-//     const totalAmountSpan = document.getElementById('total-amount');
-//     totalAmountSpan.textContent = totalAmount.toFixed(2);
-
-//     // Update the "Make Purchase" button text
-//     makePurchaseButton.textContent = `Make Purchase - ${totalAmount.toFixed(2)} TK`;
-//   }
+// adding product names 
 
 
 
 
+  // Get the ordered list element
+  const orderedList = document.getElementById("ordered-product-list");
 
-// Attach click event listeners to each "btn-card" element
-btnCards.forEach(function(btnCard) {
-  btnCard.addEventListener('click', function() {
-    // Get the product name and price from the card
-    const productName = btnCard.querySelector('.product-name').textContent;
-    const productPrice = parseFloat(btnCard.querySelector('.product-price').textContent);
+  // Function to add selected product name to the ordered list
+  function addProductToList(productName) {
+    const listItem = document.createElement("li");
+    listItem.textContent = productName;
+    orderedList.appendChild(listItem);
+    // orderedList.innerText = "";
+  }
 
-    // Create a new <li> element with the product name and number
-    const li = document.createElement('li');
-    li.textContent = `${listItemNumber}. ${productName} - ${productPrice.toFixed(2)} TK`;
-
-    // Set the data-price attribute to the <li> element for price calculation
-    li.setAttribute('data-price', productPrice);
-
-    // Append the new <li> element to the <ol> container
-    productContainer.appendChild(li);
-
-    // Increment the list item number for the next click
-    listItemNumber++;
-
-    // Update the total price after adding an item
-    updateTotalPrice();
-  });
-});
+  // Attach click event listeners to each btn-card
+  const btnCards = document.querySelectorAll(".btn-card");
+  btnCards.forEach((btnCard, index) => {
+    btnCard.addEventListener("click", () => {
+      // Replace "product-name" with the actual class of the product name element
+      const productNameElement = btnCard.querySelector(".product-name");
+      const productName = productNameElement.textContent;
+      addProductToList(`${index+1}. ${productName}`);
 
 
-// Function to calculate and update the total price
-function updateTotalPrice() {
-  // Get all the added item names and their prices
-  const addedItems = document.querySelectorAll('.added-item');
-  let totalAmount = 0;
+        
+   } ) 
 
-  // Calculate the total price
-  addedItems.forEach((item) => {
-    const price = parseFloat(item.getAttribute('data-price'));
-    totalAmount += price;
-  });
+    })
+ 
+ 
 
-  // Update the total price span
-  const totalAmountSpan = document.getElementById('total-amount');
-  totalAmountSpan.textContent = totalAmount.toFixed(2);
-
-  // Apply coupon discount if applied
-  const discount = parseFloat(document.getElementById('discount').textContent);
-  const discountedTotal = totalAmount - discount;
-  
-  // Update the total span with the discounted total
-  const totalSpan = document.getElementById('total');
-  totalSpan.textContent = discountedTotal.toFixed(2);
-
-  // Update the "Make Purchase" button text
-  makePurchaseButton.textContent = `Make Purchase - ${discountedTotal.toFixed(2)} TK`;
-}
--- js/final.js
+++ js/final.js
@@ -1,18 +1,4 @@
 
-// Function to update card selection and calculate total
-function updateCart(productName, productPrice) {
-    const cartSection = document.getElementById('cart-section');
-    const cardNameElement = document.getElementById('card-name');
-    const totalElement = document.getElementById('total-amount');
-
-    cardNameElement.textContent = productName;
-    
-    // Calculate total price with discount
-    const total = parseFloat(totalElement.dataset.originalTotal) - (parseFloat(productPrice) * 0.2);
-    totalElement.textContent = total.toFixed(2) + ' TK';
-
-    cartSection.style.display = 'block';
-}
 
 
 // Attach click event to card buttons
